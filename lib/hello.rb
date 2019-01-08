ff84591a0775aeb00bc2c79de561ad74dc26dd5a
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-01-08 04:41:57 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,11 +1,25 @@
 def hello_t(array)
-  i = 0 
-  
-  while i < array.length 
-  
-  i = i + 1 
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
 
 # call your method here!
+
+["Tim", "Tom", "Jim"]
+
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.star_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 
